3823954131c690ea0848b0889a1ed3a21a8bb5e6
Sala 3 <sala3@FI-LabSistemas.ucatolica.loc>
2025-02-24 17:12:53 -0500

    app

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Usuario from './models/usuario.js';
 
 dotenv.config();
 
@@ -8,8 +9,25 @@ const app = express();
 
 app.get("/", (req,res) => {
     res.send("HOLA MUNDO")
-})
+});
 
+app.post("/register", async (req, res) => {
+    const usuario = req.body;
+
+    if(!usuario.nombre || !usuario.email || !usuario.password || !usuario.level){
+        return res.status(400).json({succes: false, message: "Ingrese todos los datos"});
+    }
+
+    const newUsuario = new Usuario(usuario)
+
+    try {
+        await newUsuario.save();
+        res.status(201).json({succes: true, data: newUsuario})
+    } catch (error) {
+        console.error("Error al registrar el usuario", error.message)
+        res.status(500).json({succes: false, message: "Error del servidor"})
+    }
+});
 
 app.listen(5000, () => {
     connectDB();
