70c09a1eda06dbbb3e19ae34867ce0987bac6c4f
Sherlock <csanchezcabrera1@gmail.com>
2025-02-23 19:21:54 -0500

    Nuevo set

---
+++ app/backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONG_URI);
+        console.log(`Mongo DB is connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1) // 1 porque hay un error, 0 porque se estableció la conexion
+    }
+}
---
+++ app/backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("HOLA MAMI")
+})
+
+
+app.listen(5000, () => {
+
+    console.log("El servidor inicio en http://localhost:5000");
+
+});
+
