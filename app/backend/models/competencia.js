ef0d717f877116024b66e4dcd88dfecdf2794e8d
Marian <mmmartinez46@ucatolica.edu.co>
2025-03-04 16:20:02 -0500

    schema Evaluacion y Competencia

---
+++ app/backend/models/competencia.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const competenciaSchema = mongoose.Schema({
+    id_competencia: { type: ObjectId, require: true },
+    nombre: { type: String, require: true },
+    descripcion: { type: String, require: true },
+    dificultad: {type: String, enum: ["Fácil, Medio, Difícil"], require: true},
+    profesor_creador: { type: mongoose.Schema.Types.ObjectId, ref: "Usuario", require: true },
+    estudiantes_inscritos: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    fecha_creacion: { type: Date, default: Date.now }
+});
