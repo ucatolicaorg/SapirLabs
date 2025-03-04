75347738fc223f702900900198777153bd434a30
Jose <jlpena27@ucatolica.edu.co>
2025-03-04 15:32:36 -0500

    schema recompensa y Problemas

---
+++ app/backend/recompensa.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const recompensaScheme = mongoose.Schema({
+    id_recompensa: {type: ObjectId, require: true},
+    titulo_recompensa: {type: String, require: true},
+    descripcion_re: {type: String, require: true},
+    entrada_ejemplo: {type: String, require:true},
+    salida_ejemplo: {type: String, require: true},
+    dificultad: {type: String, enum: ["Fácil, Medio, Difícil"], require: true},
+    competencia_asociada: {type: mongoose.Schema.Types.ObjectId, ref: "Competencia"},
+    autor: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    genereado_por_IA: {type: Boolean, require: true},
+    Modelado_usado: {type: String, require: true},
+    fecha_creacion: {type: Date, default: Date.now},
+})
