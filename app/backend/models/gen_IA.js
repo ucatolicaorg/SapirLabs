6f39c33f9790c24d881eac98c8349a387ab8d4bd
Sherlock <csanchezcabrera1@gmail.com>
2025-03-05 16:58:42 -0500

    "Rutas y controller Maraton y gen_IA"

--- app/backend/models/gen_IA.js
+++ app/backend/models/gen_IA.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 
-const gen_IASchema = mongoose.Schema({
-    id_genIA: { type: ObjectId, required: true},
-    tipo: { type: String, enum:["Problema", "Maraton"], required: true},
-    parametros: { type: Object, required: true},
-    resultado: {type: mongoose.Schema.Types, refPath: "tipo"},
-    modelo_usado : { type: String, required: true},
-    estado: { type: String, required: true},
-    fecha_generacion: { type: Date, default: Date.now},
+const genIASchema = new mongoose.Schema({
+  tipo: { type: String, enum: ["Problema", "Maraton"], required: true },
+  parametros: { type: Object, required: true },
+  resultado: { type: mongoose.Schema.Types.ObjectId, refPath: "tipo" },
+  modelo_usado: { type: String, required: true },
+  estado: { type: String, required: true },
+  fecha_generacion: { type: Date, default: Date.now }
 });
 
-const Gen_IA = mongoose.model('Gen_IA', gen_IASchema);
+const GenIA = mongoose.model("GenIA", genIASchema);
 
-export default Gen_IA;
+export default GenIA;
