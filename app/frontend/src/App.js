812ed96459805e3473e1b99441d4981d987c1524
Marian <mmmartinez46@ucatolica.edu.co>
2025-03-06 12:56:21 -0500

    Se modifico App.js

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,23 +1,13 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
 function App() {
-  const [usuarios, setUsuarios] = useState([]);
 
-  useEffect(() => {
-    axios.get("http://localhost:5000/api/usuarios")
-      .then(response => setUsuarios(response.data))
-      .catch(error => console.error("Error al obtener usuarios:", error));
-  }, []);
 
   return (
     <div>
-      <h1>Lista de Usuarios</h1>
-      <ul>
-        {usuarios.map(user => (
-          <li key={user._id}>{user.nombre} - {user.email}</li>
-        ))}
-      </ul>
+      <h1>Login</h1>
+      <input type= "text" placeholder= "Nombre de usuario"/>
+      <input type= "text" placeholder= "Email"/>
+      <input type= "password" placeholder= "Contraseña"/>
+      <button> Iniciar sesion </button>
     </div>
   );
 }
