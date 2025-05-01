39ebf8fddbbb7c9ca5cad61a0f355a68878fa569
Sherlock <csanchezcabrera1@gmail.com>
2025-05-01 14:47:28 -0500

    admin

--- app/frontend/src/components/profNavBar.jsx
+++ app/frontend/src/components/profNavBar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-export function AdminNavBar () {
+export function ProfNavBar () {
     const navigate = useNavigate();
     const handleLogout = () => {
             localStorage.removeItem("token");
@@ -13,12 +13,12 @@ export function AdminNavBar () {
             <div className="flex flex-col items-center justify-start fixed left-0 top-0 h-screen w-64 p-6 gap-10 bg-gray-900 border border-neutral-700 rounded">
                <div className="flex items-center justify-center">
                   <img className="h-10 w-10" src="./sapirLabs.svg" alt="Logo SapirLabs" />
-                  <Link to={"/admindash"} className="text-4xl font-bold">AdminLabs</Link>
+                  <Link to={"/admindash"} className="text-4xl font-bold">SapirLabs</Link>
                 </div>
                   
                 <div className="flex items">
                     <Link to={"/admindash"}>
-                    Lista de usuarios
+                    Lista de ejercicios
                    </Link>
                 </div>
 
